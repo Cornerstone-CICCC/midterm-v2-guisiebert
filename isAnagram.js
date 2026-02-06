e518c4e7af869bf3888e843dc8d330804c425ed0
Guilherme Siebert <guilhermebsiebert@gmail.com>
2026-02-06 09:53:53 -0800

    very elegant solution for ex3

--- isAnagram.js
+++ isAnagram.js
@@ -28,9 +28,9 @@
 */
 
 function isAnagram(s, t) {
-    //Place your solution here: 
-    //return false; 
-
+    const sortedS = s.split("").sort().join()
+    const sortedT = t.split("").sort().join()
+    return sortedS == sortedT
 }
 
 // Example usage:
